058534ab6a022a6a623b86129dba4a3e030faf5b
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-04-17 05:52:38 +0200

    adding some data of teammates

---
+++ src/services/about/team.mock.js
@@ -0,0 +1,6 @@
+export const develpers=[
+    {image:require("../../../assets/Team/hasabalah.jpg"),name:"hasabalah",email:"hasabalah.@gmail.com"},
+    {image:require("../../../assets/Team/Iman.jpg"),name:"Iman Ashour",email:"Iman.@gmail.com"},
+    {image:require("../../../assets/Team/Elnawagy.jpg"),name:"Mohamed ElNawagy",email:"hasabalah.@gmail.com"},
+    {image:require("../../../assets/Team/Elkafafy.jpg"),name:"Ahmed ElKafafy",email:"hasabalah.@gmail.com"}
+  ];
