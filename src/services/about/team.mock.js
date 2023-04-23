c7787ffd610e14ab56fcb2aad1abf60298e15c4d
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2023-04-24 01:30:10 +0200

    update team data

--- src/services/about/team.mock.js
+++ src/services/about/team.mock.js
@@ -1,42 +1,42 @@
 export const develpers = [
   {
     image: require("../../../assets/Team/1.png"),
-    name: "hasabalah",
-    email: "hasabalah.@gmail.com",
+    name: "Mohamed Khatab",
+    email: "mohamedebrahimkhatab@gmail.com",
   },
   {
     image: require("../../../assets/Team/2.png"),
-    name: "Iman Ashour",
-    email: "Iman.@gmail.com",
+    name: "Faris Khatab",
+    email: "fariskhattab1@gmail.com",
   },
   {
     image: require("../../../assets/Team/3.png"),
-    name: "Mohamed ElNawagy",
-    email: "hasabalah.@gmail.com",
-  },
-  {
-    image: require("../../../assets/Team/5.png"),
-    name: "Ahmed ElKafafy",
-    email: "hasabalah.@gmail.com",
-  },
-  {
-    image: require("../../../assets/Team/6.png"),
-    name: "Ahmed ElKafafy",
-    email: "hasabalah.@gmail.com",
-  },
-  {
-    image: require("../../../assets/Team/7.png"),
-    name: "Ahmed ElKafafy",
-    email: "hasabalah.@gmail.com",
-  },
-  {
-    image: require("../../../assets/Team/8.png"),
-    name: "Ahmed ElKafafy",
-    email: "hasabalah.@gmail.com",
+    name: "Adnan Tawakol",
+    email: "adnanTemp@gmail.com",
   },
   {
     image: require("../../../assets/Team/4.png"),
-    name: "Ahmed ElKafafy",
-    email: "hasabalah.@gmail.com",
+    name: "Diaa Eltaiby",
+    email: "DiaaTemp@gmail.com",
+  },
+  {
+    image: require("../../../assets/Team/5.png"),
+    name: "AhmedHegazy",
+    email: "ahmedtemp@gmail.com",
+  },
+  {
+    image: require("../../../assets/Team/6.png"),
+    name: "Ahmed Ebrahim",
+    email: "ahmedtemp@gmail.com",
+  },
+  {
+    image: require("../../../assets/Team/7.png"),
+    name: "Mohamed Sweilam",
+    email: "mohamedtemp@gmail.com",
+  },
+  {
+    image: require("../../../assets/Team/8.png"),
+    name: "Omnia Khalil",
+    email: "onmia@gmail.com",
   },
 ];
